a5cd9ece3b825acfe64d0b28e3fa66f69a5b6cee
am22ar <amarpatil12222@gmail.com>
2022-09-27 14:36:49 +0530

    Added: Logic to calculate length of line

--- src/com/java/linecomparison/LineComaprison.java
+++ src/com/java/linecomparison/LineComaprison.java
@@ -1,7 +1,28 @@
 package com.java.linecomparison;
 
+import java.util.*;
 public class LineComaprison {
+	
+	 void lengthCalculate() {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.println("Enter x1 co-ordinate : ");
+	        int x1 = sc.nextInt();
+	        System.out.println("Enter x2 co-ordinate : ");
+	        int x2 = sc.nextInt();
+	        System.out.println("Enter y1 co-ordinate : ");
+	        int y1 = sc.nextInt();
+	        System.out.println("Enter y2 co-ordinate : ");
+	        int y2 = sc.nextInt();
+
+	        Double lenOfLineFirst = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+	        System.out.println("Length of first line is " + lenOfLineFirst);
+	    }
+
+	
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Program");
+		LineComaprison lc = new LineComaprison();
+		lc.lengthCalculate();
 	}
 }
