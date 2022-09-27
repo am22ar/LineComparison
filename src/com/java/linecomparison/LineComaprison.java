dd337fb6a9dcade35eff502b68b8fc07292476cb
am22ar <amarpatil12222@gmail.com>
2022-09-27 14:44:07 +0530

    Added: Logic to calculate length of line and Check whether they are equal or not

--- src/com/java/linecomparison/LineComaprison.java
+++ src/com/java/linecomparison/LineComaprison.java
@@ -16,6 +16,24 @@ public class LineComaprison {
 
 	        Double lenOfLineFirst = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
 	        System.out.println("Length of first line is " + lenOfLineFirst);
+	        
+	        System.out.println("Calculate length of second line");
+	        System.out.print("Enter x3 co-ordinate: ");
+	        int x3 = sc.nextInt();
+	        System.out.print("Enter x4 co-ordinate: ");
+	        int x4 = sc.nextInt();
+	        System.out.print("Enter y3 co-ordinate: ");
+	        int y3 = sc.nextInt();
+	        System.out.print("Enter y4 co-ordinate: ");
+	        int y4 = sc.nextInt();
+
+	        Double lenOfLineSecond = Math.sqrt(Math.pow((x4 - x3), 2) + Math.pow((y4 - y3), 2));
+	        System.out.println("Length of second line is " + lenOfLineSecond);
+
+	        if (lenOfLineFirst.equals(lenOfLineSecond))
+	            System.out.println("Lines are equal");
+	        else
+	            System.out.println("Lines are not equal");
 	    }
 
 	
